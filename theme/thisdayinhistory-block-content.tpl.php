8cffd9f6ba96a9d5b9794254c15370eb574d1997
Jerrick Morsey <jerricko@unm.edu>
2015-04-15 11:48:36 -0600

    init

--- theme/thisdayinhistory-block-content.tpl.php
+++ theme/thisdayinhistory-block-content.tpl.php
@@ -1,10 +1,9 @@
 <?php
+// $Id: thisdayinhistory-block-content.tpl.php,v 1.1 2009/02/10 19:40:01 dmcgladdery Exp $
 /**
  * @file
  * Default template for TDiH block content
  */
-
-  drupal_add_css(drupal_get_path('module', 'thisdayinhistory') .'/thisdayinhistory.css');
 
   foreach ($node_list as $node) {
     $nodes_by_year[$node->year][] = $node;
@@ -12,13 +11,15 @@
 
   foreach ($nodes_by_year as $year => $year_list) {
     foreach ($year_list as $node) {
+      $n = node_view($node,'teaser');
+      $body = field_get_items('node', $node, 'body');
       $row = array(
         array('data' => '<p>'. $node->year .':</p>', 'valign' => 'top', 'class' => 'year-column'),
-        array('data' => $node->teaser),
+        array('data' => text_summary($body[0]['value'])),
       );
       $tablerows[] = $row;
     }
   }
 
   $attributes = array('class' => 'tdih-events');
-  print $block['header'] . theme('table', array(), $tablerows, $attributes);
+  print $block['header'] . theme('table', array('header' => array(), 'rows' => $tablerows, 'attributes' => $attributes));
